64026d86c7205f12be92aa9de6abbfb885b7188f
PerFilip <filipheden@gmail.com>
2019-02-27 18:20:40 +0000

    complete

---
+++ Lab5/java/labSelfMade.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Collections;
+import java.io.IOException;
+
+public class labSelfMade
+{
+
+    public static void main(String[] args) 
+    {
+        LinkedList<Integer> numbers = new LinkedList<>();
+        String readFileName = args[0];
+        File readFile = new File(readFileName + ".txt");
+        String writeFileName = args[1];
+        File writeFile = new File(writeFileName + ".txt");
+        int N = Integer.parseInt(args[2]);
+        BufferedReader br;
+        FileWriter fr;
+
+        try {
+            if (!writeFile.createNewFile()) {
+                System.out.println("Couldn't create file! (FileName already exists)");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            br = new BufferedReader(new FileReader(readFile));
+            String currRow = br.readLine();
+
+            while(currRow != null) 
+                {
+                    numbers.add(Integer.parseInt(currRow));
+                    currRow = br.readLine();
+                }
+
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            fr = new FileWriter(writeFile);
+            fr.write("N = " + N + System.lineSeparator());
+
+            for (int n = 0; n <= N; n++) 
+            {
+                LinkedList<Integer> newCopiedList = (LinkedList<Integer>) numbers.clone();
+                long startTime = System.currentTimeMillis();
+                ListSorter.sort(newCopiedList);
+                long stopTime = System.currentTimeMillis();
+
+                long time = stopTime - startTime;
+                fr.write("Time measurement " + n + ": " + time + " ms" + System.lineSeparator());
+            }
+
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
